8e9add2f0978bf201988bf9adb4aa7fff205d287
Emagip <emagip@pm.me>
2025-02-21 12:01:16 +0100

    Un début de tk, pas mal opti

---
+++ LeTk.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+
+WIDTH, HEIGHT = 800, 600
+X_OFFSET, Y_OFFSET = WIDTH // 2, HEIGHT // 2
+SCALE = 40
+
+def transform(x, y):
+    return X_OFFSET + x * SCALE, Y_OFFSET - y * SCALE
+
+root = tk.Tk()
+root.title("Le Jeu des cercles")
+
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="white")
+canvas.pack()
+
+#x0, y0 = transform(4 - 4, 0 + 4)
+#x1, y1 = transform(4 + 4, 0 - 4)
+#canvas.create_oval(x0, y0, x1, y1, outline="red", width=2)
+
+couleurs = [None, None, "white", "red", "blue"]
+x0, y0 = transform(-2.4 - 5.6, 0 + 5.6)
+x1, y1 = transform(-2.4 + 5.6, 0 - 5.6)
+coords_set = {
+        "c0": [[transform(-5.95 - 8.67, 0 + 8.67), transform(-5.95 + 8.67, 0 - 8.67), 23.51, -47.02], [transform(4 - 4, 0 + 4), transform(4 + 4, 0 - 4), 120.03, 119.94],2],
+        "c1": [[transform(-2.4 - 8.4, -10.62 + 8.4), transform(-2.4 + 8.4, -10.62 - 8.4), 58.41, 63.18],[(x0, y0), (x1, y1),-38.17,-103.66],3],
+        "c2": [[transform(-10.22 - 4.87, 0 + 4.87), transform(-10.22 + 4.87, 0 - 4.87), 45.33, -90.65],[(x0,y0), (x1, y1), 141.83, 76.34], 3],
+        "c3": [[transform(-2.4 - 8.4, 10.62 + 8.4), transform(-2.4 + 8.4, 10.62 - 8.4), -58.41, -63.18],[(x0,y0), (x1,y1),38.17, 103.66], 3]
+        }
+traced = {}
+for nom, info in coords_set.items():
+    traced[nom] = [
+            canvas.create_arc(info[0][0][0], info[0][0][1], info[0][1][0], info[0][1][1], outline=couleurs[info[2]], style='chord', fill=couleurs[info[2]], start=info[0][2],extent = info[0][3]),
+            canvas.create_arc(info[1][0][0], info[1][0][1], info[1][1][0], info[1][1][1], outline=couleurs[info[2]], style='chord', fill=couleurs[info[2]], start=info[1][2],extent = info[1][3])
+        ]
+    canvas.create_arc(info[0][0][0], info[0][0][1], info[0][1][0], info[0][1][1], outline='black', style='arc', width=3, start=info[0][2],extent = info[0][3])
+    canvas.create_arc(info[1][0][0], info[1][0][1], info[1][1][0], info[1][1][1], outline='black', style='arc', width=3, start=info[1][2],extent = info[1][3])
+
+print(traced)
+root.mainloop()
