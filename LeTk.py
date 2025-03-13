723ccd7abc5abfa0e9b23c4ab226fba934411465
Emagip <emagip@pm.me>
2025-03-13 11:21:15 +0100

    Fin des test en tk, je passe a l'intégration en jeu

--- LeTk.py
+++ LeTk.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+import time
 
-WIDTH, HEIGHT = 1920, 1080
+WIDTH, HEIGHT = 800, 600
 X_OFFSET, Y_OFFSET = WIDTH // 2, HEIGHT // 2
-SCALE = 50
+SCALE = 40
 
 def transform(x, y):
     return X_OFFSET + x * SCALE, Y_OFFSET - y * SCALE
@@ -10,9 +11,15 @@ def transform(x, y):
 root = tk.Tk()
 root.title("Le Jeu des cercles")
 
-canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="white")
-canvas.pack()
+frame1 = tk.Frame(root)
+frame1.pack(side = "left")
 
+frame2 = tk.Frame(root)
+frame2.pack(side="right")
+canvas1 = tk.Canvas(frame1, width=WIDTH, height=HEIGHT, bg="white")
+canvas1.pack()
+canvas2 = tk.Canvas(frame2, width=WIDTH, height=HEIGHT, bg="white")
+canvas2.pack()
 #x0, y0 = transform(4 - 4, 0 + 4)
 #x1, y1 = transform(4 + 4, 0 - 4)
 #canvas.create_oval(x0, y0, x1, y1, outline="red", width=2)
@@ -28,41 +35,104 @@ coords_set = {
         "c4": [[transform(9.01 - 8.71, 8.66 + 8.71), transform(9.01 + 8.71, 8.66 - 8.71), -96.65, -46.78],[transform(4 - 4, 0 + 4), transform(4 + 4, 0 - 4), 0, 120.03],4],
         "c5": [[transform(9.01 - 8.71, -8.66 + 8.71), transform(9.01 + 8.71, -8.66 - 8.71), 96.65, 46.78],[transform(4 - 4, 0 + 4), transform(4 + 4, 0 - 4), 0, -120.03],4]
         }
-traced = {}
+traced1 = {}
+traced2 = {}
 for nom, info in coords_set.items():
-    traced[nom] = [
-            canvas.create_arc(info[0][0][0], info[0][0][1],
+    traced1[nom] = [
+            canvas1.create_arc(info[0][0][0], info[0][0][1],
                               info[0][1][0], info[0][1][1],
                               outline=couleurs[info[2]], fill=couleurs[info[2]],
                               style='chord',
                               start=info[0][2],extent = info[0][3]),
 
-            canvas.create_arc(info[1][0][0], info[1][0][1],
+            canvas1.create_arc(info[1][0][0], info[1][0][1],
                               info[1][1][0], info[1][1][1],
                               outline=couleurs[info[2]], fill=couleurs[info[2]],
                               style='chord', 
                               start=info[1][2],extent = info[1][3])
+            ]
+    traced2[nom] = [
+            canvas2.create_arc(info[0][0][0], info[0][0][1],
+                              info[0][1][0], info[0][1][1],
+                              outline=couleurs[info[2]], fill=couleurs[info[2]],
+                              style='chord',
+                              start=info[0][2],extent = info[0][3]),
+
+            canvas2.create_arc(info[1][0][0], info[1][0][1],
+                              info[1][1][0], info[1][1][1],
+                              outline=couleurs[info[2]], fill=couleurs[info[2]],
+                              style='chord', 
+                              start=info[1][2],extent = info[1][3])
+
         ]
-    canvas.create_arc(info[0][0][0], info[0][0][1],
+    canvas1.create_arc(info[0][0][0], info[0][0][1],
                       info[0][1][0], info[0][1][1],
                       outline='black', style='arc',
                       width=3,
                       start=info[0][2],extent = info[0][3])
 
-    canvas.create_arc(info[1][0][0], info[1][0][1],
+    canvas1.create_arc(info[1][0][0], info[1][0][1],
+                      info[1][1][0], info[1][1][1],
+                      outline='black', style='arc',
+                      width=3,
+                      start=info[1][2],extent = info[1][3])
+    canvas2.create_arc(info[0][0][0], info[0][0][1],
+                      info[0][1][0], info[0][1][1],
+                      outline='black', style='arc',
+                      width=3,
+                      start=info[0][2],extent = info[0][3])
+
+    canvas2.create_arc(info[1][0][0], info[1][0][1],
                       info[1][1][0], info[1][1][1],
                       outline='black', style='arc',
                       width=3,
                       start=info[1][2],extent = info[1][3])
 
-print(traced)
-position = [4,3,4,3,3,2]
-def change(pos):
-    i = 0
-    for nom, obj in traced.items():
-        canvas.itemconfig(obj[0], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
-        canvas.itemconfig(obj[1], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
-        i+=1
 
-root.after(1000, change(position))
+
+#x0, y0 = transform(4 - 4, 0 + 4)
+#x1, y1 = transform(4 + 4, 0 - 4)
+#canvas.create_oval(x0, y0, x1, y1, outline="red", width=2)
+print(traced2)
+def change1(pos):
+    i = 0
+    for nom, obj in traced1.items():
+        canvas1.itemconfig(obj[0], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
+        canvas1.itemconfig(obj[1], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
+        i+=1
+    root.update()
+
+
+def change2(pos):
+    i = 0
+    for nom, obj in traced2.items():
+        canvas2.itemconfig(obj[0], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
+        canvas2.itemconfig(obj[1], fill=couleurs[pos[i]], outline=couleurs[pos[i]])
+        i+=1
+    root.update()
+
+#while True:
+#    position = change1([3,2,3,3,4,4])
+#    time.sleep(0.5)
+#    position = change1([3,3,2,3,4,4])
+#    time.sleep(0.5)
+#    position = change1([4,3,2,3,3,4])
+#    time.sleep(0.5)
+#    position = change1([4,3,2,3,4,3])
+#    time.sleep(0.5)
+#    change1([3,4,3,2,4,3])
+#    time.sleep(0.5)
+#    change1([2,3,4,3,4,3])
+#    time.sleep(0.5)
+#    change1([3,2,3,4,4,3])
+#    time.sleep(0.5)
+#    change1([4,3,2,3,4,3])
+#    time.sleep(0.5)
+#    change1([3,3,2,3,4,4])
+#    time.sleep(0.5)
+#    change1([3,3,3,2,4,4])
+#    time.sleep(0.5)
+#    change1([2,3,3,3,4,4])
+#    time.sleep(0.5)
+
 root.mainloop()
